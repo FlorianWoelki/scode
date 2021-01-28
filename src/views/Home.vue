16a14bb6a19bc3fca6da862ac65e7aedd77d7bdc
Florian Woelki <florianwoelki@gmx.de>
2021-01-28 13:39:25 +0100

    feat(view): add columns

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Hello Home</h1>
+  <div class="grid h-screen grid-cols-4">
+    <div class="col-span-1 bg-gray-700">abc</div>
+    <div class="col-span-3 bg-gray-600">test</div>
+  </div>
 </template>
 
 <script lang="ts">
