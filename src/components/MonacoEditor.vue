abc98cbcb9eedafe117c5f37debeeb1668bd7f0e
Florian Woelki <florianwoelki@gmx.de>
2021-01-29 14:37:26 +0100

    feat(comp): add loading animation to MonacoEditor

--- src/components/MonacoEditor.vue
+++ src/components/MonacoEditor.vue
@@ -1,4 +1,10 @@
 <template>
+  <div v-if="isLoading" class="flex items-center justify-center mt-4">
+    <svg class="w-6 h-6 text-gray-500 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  </div>
   <div class="w-full h-auto mt-4 rounded-lg" ref="monacoElement"></div>
 </template>
 
@@ -11,9 +17,11 @@ import DraculaTheme from '../monaco/DraculaTheme';
 export default defineComponent({
   setup() {
     const monacoElement = ref<HTMLElement | null>(null);
+    const isLoading = ref(true);
 
     onMounted(() => {
       loader.init().then((monaco) => {
+        isLoading.value = false;
         if (monacoElement.value) {
           const editor = monaco.editor.create(monacoElement.value, {
             value: 'console.log("Hello World");',
@@ -49,6 +57,7 @@ export default defineComponent({
 
     return {
       monacoElement,
+      isLoading,
     };
   },
 });
