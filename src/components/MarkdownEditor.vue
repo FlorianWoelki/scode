a84e490bbf253a1ca18ef683b096103c8162d4e1
Florian Woelki <florianwoelki@gmx.de>
2021-02-04 19:27:03 +0100

    fix: markdown content and placeholder for quill editor

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -12,9 +12,15 @@ export default defineComponent({
     const editor = ref<any>(null);
 
     onMounted(() => {
-      const quillEditor = new Quill(editor.value, {});
+      const quillEditor = new Quill(editor.value, {
+        placeholder: 'Write your markdown here...',
+      });
       /* eslint-disable-next-line */
       new QuillMarkdown(quillEditor);
+
+      if (!quillEditor.root.classList.contains('markdown')) {
+        quillEditor.root.classList.add('markdown');
+      }
     });
 
     return {
@@ -24,10 +30,16 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
+<style>
 @import '../styles/markdown.scss';
 
 .ql-editor {
-  @apply focus:outline-none text-white markdown;
+  @apply focus:outline-none text-white;
+}
+.ql-editor.ql-blank::before {
+  @apply text-gray-500;
+}
+.ql-clipboard {
+  @apply hidden;
 }
 </style>
