2426e9cc756887ce486ac706f71d3aca39d59eaf
Florian Woelki <florianwoelki@gmx.de>
2021-02-07 18:12:05 +0100

    feat(comp): add inline block code functionality

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -9,7 +9,6 @@
 </template>
 
 <script lang="ts">
-import { resolvePackageEntry } from 'vite';
 import { defineComponent, nextTick, onMounted, ref, watch } from 'vue';
 
 export default defineComponent({
@@ -67,33 +66,40 @@ export default defineComponent({
       }
     };
 
+    const boldPattern = /(\*|_){2}(.+?)(?:\1){2}/g;
+    const italicPattern = /(\*|_){1}(.+?)(?:\1){1}/g;
+    const unorderedListPattern = /^\s{0,9}(-|\*){1}\s/;
+    const headerPattern = /^(#){1,6}\s/g;
+    const inlineCodePattern = /(`){1}(.+?)(`){1}/g;
+
     const replaceHeaderCharacters = (target: HTMLElement): void => {
       setTimeout(() => {
         target.innerHTML = target.innerHTML.replace(/[#]{1,6}/g, '').replace('&nbsp;', '<br>');
         placeCaretAtEnd(target);
-      });
+      }, 1);
     };
 
     const replaceUnorderedListCharacters = (target: HTMLElement): void => {
       setTimeout(() => {
-        target.innerHTML = target.innerHTML.replace(/\*/gm, '').replace('&nbsp;', '<br>');
+        target.innerHTML = target.innerHTML.replace(/\*/gm, '').replace(/^(&nbsp;)(?!\w)/gm, '<br>');
         placeCaretAtEnd(target);
-      });
+      }, 1);
     };
 
     const replaceOrderedListCharacters = (target: HTMLElement): void => {
       setTimeout(() => {
         target.innerHTML = target.innerHTML.replace(/1\./gm, '').replace('&nbsp;', '<br>');
         placeCaretAtEnd(target);
-      });
+      }, 1);
     };
 
     const handleKeydown = (e: KeyboardEvent) => {
       const target = e.target as HTMLElement;
-      const currentInput = document.getSelection()!.anchorNode!.textContent!;
+      const selection = document.getSelection()!;
+      const currentInput = selection.anchorNode!.textContent!;
 
       if (e.key.toLowerCase() === ' ') {
-        if (currentInput.startsWith('*')) {
+        /* if (currentInput.startsWith('*')) {
           nextTick(() => exec('insertUnorderedList'));
           replaceUnorderedListCharacters(target);
         }
@@ -101,9 +107,9 @@ export default defineComponent({
         if (currentInput.startsWith('1.')) {
           nextTick(() => exec('insertOrderedList'));
           replaceOrderedListCharacters(target);
-        }
+        } */
 
-        if (/_[A-Za-z0-9]+_/gim.test(currentInput)) {
+        /* if (/_[A-Za-z0-9]+_/gim.test(currentInput)) {
           const italic = /_(.*?)_/gm;
           const html = target.innerHTML.replace(italic, '<i>$1</i>');
           nextTick(() => exec('italic'));
@@ -111,23 +117,89 @@ export default defineComponent({
           placeCaretAtEnd(target);
         } else if (queryCommandValue('italic') !== 'false') {
           nextTick(() => exec('italic'));
+        } */
+
+        // inline code
+        const inlineCodeMatch = inlineCodePattern.exec(currentInput);
+        if (inlineCodeMatch) {
+          const [annotatedText] = inlineCodeMatch;
+          setTimeout(() => {
+            const message = annotatedText.replace(/`/g, '');
+            target.innerHTML = target.innerHTML.replace(currentInput.substring(inlineCodeMatch.index), `<code>${message}</code>`);
+            placeCaretAtEnd(target);
+          }, 0);
         }
 
-        if (/\*\*(.*?)\*\*/gm.test(currentInput)) {
-          const bold = /\*\*(.*?)\*\*/gm;
-          const html = target.innerHTML.replace(bold, '<strong>$1</strong>');
-          nextTick(() => exec('bold'));
-          target.innerHTML = html;
-          placeCaretAtEnd(target);
+        // headers
+        const headerMatch = headerPattern.exec(currentInput);
+        if (headerMatch) {
+          const size = headerMatch[0].trim().length;
+          setTimeout(() => {
+            target.innerHTML = target.innerHTML.replace(currentInput, currentInput.slice(size + 1));
+            placeCaretAtEnd(target);
+            nextTick(() => exec('formatBlock', `<h${size}>`));
+          }, 0);
+        }
+
+        // unordered list
+        const unorderedListMatch = unorderedListPattern.exec(currentInput);
+        if (unorderedListMatch) {
+          if (currentInput.split('- ')[1] || currentInput.split('* ')[1]) {
+            setTimeout(() => {
+              const offsetText = /^\s{0,9}(\*){1}\s/.test(currentInput) ? currentInput.replace('*', '-') : currentInput;
+              // const depth = offsetText.split('- ')[0].split('').filter((e) => /\s/gi.test(e)).length;
+              const replaceText = offsetText.split('- ').length > 1 ? offsetText.split('- ').splice(1, 1).join('') : offsetText;
+              target.innerHTML = target.innerHTML.replace(currentInput, replaceText);
+              placeCaretAtEnd(target);
+              nextTick(() => {
+                exec('insertUnorderedList');
+                placeCaretAtEnd(target);
+              });
+            }, 0);
+          }
+        }
+
+        // italic
+        const italicMatch = italicPattern.exec(currentInput);
+        if (italicMatch) {
+          const [annotatedText, , matchedText] = italicMatch;
+          if (currentInput[italicMatch.index] !== currentInput[italicMatch.index + 1]) {
+            setTimeout(() => {
+              target.innerHTML = target.innerHTML.replace(annotatedText, `<i>${matchedText}</i>`);
+              placeCaretAtEnd(target);
+            }, 0);
+          }
+        } else if (queryCommandValue('italic') !== 'false') {
+          nextTick(() => exec('italic'));
+        }
+
+        // bold
+        const boldMatch = boldPattern.exec(currentInput);
+        if (boldMatch) {
+          const [annotatedText, , matchedText] = boldMatch;
+          if (!currentInput.match(/^([*_ \n]+)$/g)) {
+            setTimeout(() => {
+              target.innerHTML = target.innerHTML.replace(annotatedText, `<strong>${matchedText}</strong>`);
+              placeCaretAtEnd(target);
+            }, 0);
+          }
         } else if (queryCommandValue('bold') !== 'false') {
           nextTick(() => exec('bold'));
         }
+
+        // const html = target.innerHTML.replace(boldPattern, '<strong>$1</strong>');
+        // nextTick(() => exec('bold'));
+        // target.innerHTML = html;
+        // placeCaretAtEnd(target);
+        /* } else if (queryCommandValue('bold') !== 'false') {
+          nextTick(() => exec('bold'));
+        } */
 
         if (currentInput.startsWith('>')) {
           nextTick(() => exec('formatBlock', 'blockquote'));
         }
 
-        if (currentInput.startsWith('######')) {
+        /* if (currentInput.startsWith('######')) {
           nextTick(() => exec('formatBlock', '<h6>'));
           replaceHeaderCharacters(target);
         } else if (currentInput.startsWith('#####')) {
@@ -145,7 +217,7 @@ export default defineComponent({
         } else if (currentInput.startsWith('#')) {
           nextTick(() => exec('formatBlock', '<h1>'));
           replaceHeaderCharacters(target);
-        }
+        } */
       }
 
       /* if (currentInput === '' && queryCommandValue('insertUnorderedList') === 'false') {
