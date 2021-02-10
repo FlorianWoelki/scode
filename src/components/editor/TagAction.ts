1097704fef4c923bb5dfdabaea0d00538c53f8c0
Florian Woelki <florianwoelki@gmx.de>
2021-02-10 17:08:25 +0100

    ref(comp): remove unused method

--- src/components/editor/TagAction.ts
+++ src/components/editor/TagAction.ts
@@ -5,20 +5,6 @@ export abstract class TagAction {
 
   public abstract run(input: string, target: HTMLElement): void;
 
-  protected placeCaretAtEnd(target: HTMLElement): void {
-    /* let range: Range;
-    let sel: Selection | null;
-    target.focus();
-    if (typeof window.getSelection !== 'undefined' && typeof document.createRange !== 'undefined') {
-      range = document.createRange();
-      range.selectNodeContents(target);
-      range.collapse(false);
-      sel = window.getSelection();
-      sel?.removeAllRanges();
-      sel?.addRange(range);
-    } */
-  }
-
   protected exec(command: string, value?: string) {
     return document.execCommand(command, false, value);
   }
