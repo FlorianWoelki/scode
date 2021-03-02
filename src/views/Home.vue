7aecf82274250206227d1d1164c38fcf6243cc2b
Florian Woelki <florianwoelki@gmx.de>
2021-03-02 13:44:01 +0100

    chore(view): change design of spaces

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,50 @@
 <template>
   <div class="grid h-screen grid-cols-12">
-    <div class="col-span-2 bg-gray-700">
-      <!-- SPACES HEADER -->
-      <div class="flex items-center justify-between text-gray-400 uppercase pt-7 px-7">
-        <div class="flex items-center space-x-2">
-          <CollectionIcon class="w-5 h-5 text-gray-600" />
-          <p class="text-xs font-bold tracking-wider">Spaces</p>
+    <div class="z-10 col-span-3 bg-gray-800 shadow-2xl">
+      <div class="grid h-full grid-cols-12">
+        <!-- CODE SPACES -->
+        <div class="col-span-3 p-1 mt-6 space-y-4">
+          <div class="flex flex-col items-center justify-center mx-auto">
+            <div class="flex items-center justify-center w-12 h-12 bg-gray-600 border-2 border-gray-400 rounded-lg">
+              <p class="font-light text-white">JS</p>
+            </div>
+            <p class="mt-1 text-sm font-light text-gray-400">JavaScript</p>
+          </div>
+          <div class="flex flex-col items-center justify-center mx-auto">
+            <div class="flex items-center justify-center w-12 h-12 bg-gray-600 rounded-lg">
+              <p class="font-light text-white">JS</p>
+            </div>
+          </div>
+          <PlusIcon class="flex justify-center w-8 h-8 mx-auto text-gray-600" />
         </div>
-        <div class="cursor-pointer hover:text-gray-300">
-          <PlusIcon class="w-5 h-5" />
-        </div>
-      </div>
 
-      <!-- DIRECTORIES AND SPACES -->
-      <div class="mt-10 space-y-6">
-        <div class="space-y-1">
-          <div class="flex items-center mb-3 space-x-2 text-gray-100 mx-7">
-            <FolderOpenIcon class="w-5 h-5" />
-            <p class="text-base">JavaScript</p>
+        <!-- CODE SNIPPETS -->
+        <div class="col-span-9 border-l border-gray-700">
+          <div class="flex items-center justify-between pt-7 px-7">
+            <div class="flex items-center space-x-2">
+              <FolderOpenIcon class="w-5 h-5 text-gray-600" />
+              <p class="text-base text-gray-400">JavaScript</p>
+            </div>
+            <PlusIcon class="w-5 h-5 text-gray-400 cursor-pointer hover:text-gray-300" @click="addFile" />
           </div>
-          <div class="flex items-center py-2 pl-8 space-x-2 bg-gray-800">
-            <div class="w-2 h-2 bg-green-400 rounded-full ml-7"></div>
-            <p class="text-base text-gray-100">Basics</p>
-          </div>
-          <div class="flex items-center py-1 pl-8 space-x-2 mx-7">
-            <div class="w-2 h-2 bg-red-400 rounded-full"></div>
-            <p class="text-base text-gray-500">Advanced</p>
-          </div>
-        </div>
-        <div class="space-y-2">
-          <div class="flex items-center space-x-2 text-gray-500 mx-7">
-            <FolderIcon class="w-5 h-5" />
-            <p class="text-base">C++</p>
-          </div>
-        </div>
-        <div class="ml-1 space-y-2">
-          <div class="flex items-center py-1 space-x-2 mx-7">
-            <div class="w-2 h-2 bg-red-400 rounded-full"></div>
-            <p class="text-base text-gray-500">AWS</p>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="z-10 col-span-2 bg-gray-800 shadow-2xl">
-      <!-- SNIPPETS HEADER -->
-      <div class="flex items-center justify-between pt-7 px-7">
-        <div class="flex items-center space-x-2">
-          <FolderOpenIcon class="w-5 h-5 text-gray-600" />
-          <p class="text-base text-gray-400">JavaScript/Basics</p>
+          <div class="mt-4">
+            <p v-if="files.length === 0" class="flex items-center justify-center text-sm italic text-gray-600">No files created</p>
+            <SnippetFile
+              v-else
+              v-for="file in files"
+              :key="file.id"
+              :name="file.name"
+              :content="file.blocks.length > 0 ? file.blocks[0].savedValue || '' : ''"
+              :isSelected="file.id === selectedFile?.id"
+              @click="setSelectedFile(file)"
+            />
+          </div>
         </div>
-        <PlusIcon class="w-5 h-5 text-gray-400 cursor-pointer hover:text-gray-300" @click="addFile" />
-      </div>
-
-      <div class="mt-4">
-        <p v-if="files.length === 0" class="flex items-center justify-center text-sm italic text-gray-600">No files created</p>
-        <SnippetFile
-          v-else
-          v-for="file in files"
-          :key="file.id"
-          :name="file.name"
-          :content="file.blocks.length > 0 ? file.blocks[0].savedValue || '' : ''"
-          :isSelected="file.id === selectedFile?.id"
-          @click="setSelectedFile(file)"
-        />
       </div>
     </div>
 
-    <div class="relative col-span-8 pb-32 bg-gray-800">
+    <div class="relative col-span-9 pb-32 bg-gray-800">
       <div class="w-7/12 m-auto py-7">
         <ContentDisplay
           v-if="selectedFile"
@@ -87,10 +64,8 @@
 import { computed, defineComponent } from 'vue';
 import { useStore } from 'vuex';
 import { uuid } from 'vue-uuid';
-import CollectionIcon from '../assets/icons/collection.svg';
 import PlusIcon from '../assets/icons/plus.svg';
 import FolderOpenIcon from '../assets/icons/folder-open.svg';
-import FolderIcon from '../assets/icons/folder.svg';
 import SnippetFile from '../components/SnippetFile.vue';
 import ContentDisplay from '../components/ContentDisplay.vue';
 import { IFile } from '../db';
@@ -99,10 +74,8 @@ import { AllMutationTypes } from '../store/mutation-types';
 
 export default defineComponent({
   components: {
-    CollectionIcon,
     PlusIcon,
     FolderOpenIcon,
-    FolderIcon,
     SnippetFile,
     ContentDisplay,
   },
