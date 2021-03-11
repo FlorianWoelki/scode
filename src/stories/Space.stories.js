0ea017eaf29b8966c07112866dce2c0228429977
Florian Woelki <florianwoelki@gmx.de>
2021-03-11 09:28:55 +0100

    docs(storybook): add Space story

---
+++ src/stories/Space.stories.js
@@ -0,0 +1,29 @@
+import Space from '../components/Space.vue';
+
+export default {
+  title: 'Example/Space',
+  component: Space,
+  argTypes: {
+    isSelected: { type: 'boolean' },
+    name: {
+      type: { name: 'string', required: true },
+      defaultValue: 'JavaScript',
+      control: { type: 'text' },
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { Space },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: '<Space v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'JavaScript',
+};
